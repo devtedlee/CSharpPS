11b4963dbc7401ceb28f5eca1d2d22fa2ff419c4
Ted <persisne@gmail.com>
2021-01-25 01:00:52 +0900

    add a leetcode/easy solution file

---
+++ CSharpProblemSolvingArchive/LeetCode/Easy/Q709.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace CSharpProblemSolvingArchive.LeetCode.Easy
+{
+    /// <summary>
+    /// 709. To Lower Case
+    /// https://leetcode.com/problems/to-lower-case/
+    /// </summary>
+    public sealed class Q709
+    {
+        public string ToLowerCase(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char s in str)
+            {
+                if (s >= 'A' && s <= 'Z')
+                    sb.Append((char)(s + 32));
+                else
+                    sb.Append(s);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
