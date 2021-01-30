48c2073b40d6ce2613c9f8f281d0c6cd74a45e83
Ted <persisne@gmail.com>
2021-01-30 22:46:47 +0900

    add a leetcode/easy solution file

---
+++ CSharpProblemSolvingArchive/LeetCode/Easy/Q1534.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharpProblemSolvingArchive.LeetCode.Easy
+{
+    /// <summary>
+    /// 1534. Count Good Triplets
+    /// https://leetcode.com/problems/count-good-triplets/
+    /// </summary>
+    public sealed class Q1534
+    {
+        public int CountGoodTriplets(int[] arr, int a, int b, int c)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length - 2; ++i)
+                for (int j = i + 1; j < arr.Length - 1; ++j)
+                    for (int k = j + 1; k < arr.Length; ++k)
+                        if (Math.Abs(arr[i] - arr[j]) <= a && Math.Abs(arr[j] - arr[k]) <= b && Math.Abs(arr[i] - arr[k]) <= c)
+                            ++count;
+
+            return count;
+        }
+    }
+}
